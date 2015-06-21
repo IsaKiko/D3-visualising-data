96ba5c5cd98b4ba77158030a3f44a9413d2b304e
IsaKiko <isa.kiral@gmail.com>
2015-06-22 07:22:56 +1000

    code and iframe 07

---
+++ code/main07.js
@@ -0,0 +1,27 @@
+// Load the data.
+d3.json("nations.json", function(nations) {
+
+	// Create the SVG frame inside chart_area.
+	var chart_area = d3.select("#chart_area");
+	var frame = chart_area.append("svg");
+
+	// Create canvas inside frame.
+	var canvas = frame.append("g");
+
+	// Set margins, width, and height.
+	var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5};
+	var frame_width = 960;
+	var frame_height = 350;
+	var canvas_width = frame_width - margin.left - margin.right;
+	var canvas_height = frame_height - margin.top - margin.bottom;
+
+	
+	// Set svg attributes width and height.
+	frame.attr("width", frame_width);
+	frame.attr("height", frame_height);
+
+
+	// Shift the chart and make it slightly smaller than the svg canvas.
+	canvas.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+})
